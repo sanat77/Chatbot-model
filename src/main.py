f01fd7ef5187fe36422a63f34f50705521d66be3
Sanat Bhalla <sanat@Sanats-MacBook-Pro.local>
2023-01-02 23:42:26 -0500

    orphan code removed

--- src/main.py
+++ src/main.py
@@ -2,6 +2,3 @@ from Chatbot.api import app
 
 if __name__ == "__main__":
     app.run(debug=False, port=3500)
-    # user_question = "What is the capital of USA?"
-    # reply = chatbot.get_reply(user_message=user_question)
-    # print(reply)
